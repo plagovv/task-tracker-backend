f6974b2f5ccb53fb3cbbf7aae8f7730d46309a3d
plagov <vnt2433@gmail.com>
2024-10-10 14:03:38 +0300

    Исправления для компоуза и добавлена коллекция Postman и добавлена миграция

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/statuses', statusRoutes); // Подключаем маршруты статусов
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
